6605dd3035ebf23092f60ed57cc7a95c666056d4
rafia123-gif <rafiabasharat001@gmail.com>
2025-03-02 23:56:23 +0500

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,9 @@
+print("Hello,World!")
+x=1
+#the initial value of x is 1
+if x>0:
+    print("These are two comments")
+#Input statements:
+txt=input("Type something to test this out")
+print(txt)
+print("Im Rafia");print("Im an Honest girl")
